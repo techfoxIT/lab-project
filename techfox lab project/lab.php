d9b1cfdf45216c50365b9eff6b401ac0950764f1
techfoxIT <djvinit18@gmail.com>
2017-06-16 13:08:25 +0530

    adding database outside folder

---
+++ techfox lab project/lab.php
@@ -0,0 +1,39 @@
+
+
+<?php
+
+//***********************************************//
+//* pathology lab                  *//
+//*---------------------------------------------*//
+//* Author: Manish Tiwari & Vinit Tiwari 
+//*** Title: Database  Functions           ***
+//***************************************************
+//
+
+include_once 'db.php';
+
+$conn=false;
+
+function executeQuery($query)
+{
+    global $conn,$dbserver,$dbname,$dbpassword,$dbusername;
+    global $message;
+    if (!($conn = @mysql_connect ($dbserver,$dbusername,$dbpassword)))
+         $message="Cannot connect to server";
+    if (!@mysql_select_db ($dbname, $conn))
+         $message="Cannot select database";
+
+    $result=mysql_query($query,$conn);
+    if(!$result)
+        $message="Error while executing query.<br/>Mysql Error: ".mysql_error();
+    else
+        return $result;
+
+}
+function closedb()
+{
+    global $conn;
+    if(!$conn)
+    mysql_close($conn);
+}
+?>
